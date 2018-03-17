d4595f22f56919516b6b056ba27a9cad2a6a34c9
Alexis Robillart <alexisrobillart@gmail.com>
2018-03-17 18:36:53 +0100

    fix navbar

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   post 'users/:id/send_request', to: "friends#send_request", as: "send_request"
   get 'users/:id/friends', to: "friends#index", as: "user_friends"
   post 'users/friends/remove/:id', to: "friends#destroy"
+  post 'users/:id/friends/remove/:id', to: "friends#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
